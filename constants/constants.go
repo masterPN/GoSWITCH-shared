dcd749a3efe5d47be4c11a05fe029269dfe26760
masterPN <69779985+masterPN@users.noreply.github.com>
2024-12-02 22:07:22 +0700

    fix typo and add phrases

--- constants/constants.go
+++ constants/constants.go
@@ -3,8 +3,11 @@ package constants
 const (
 	MockTimeString = "2022-01-01 10:00:00"
 
-	ExpectedErrorPhrase   = "Expected no error, but got %v"
-	RedisClosedPhrase     = "redis: client is closed"
-	ExpectErrorPhrase     = "Expected error, but got nil"
-	MissExpectationPhrase = "Not all expectations were met: %v"
+	ExpectNoErrorPhrase     = "Expected no error, but got %v"
+	RedisClosedPhrase       = "redis: client is closed"
+	ExpectErrorPhrase       = "Expected error, but got nil"
+	MissExpectationPhrase   = "Not all expectations were met: %v"
+	FailedJsonBindingPhrase = "failed JSON binding"
+	EmptyInputPhrase        = "empty input"
+	ErrorPhrase             = "some error"
 )
